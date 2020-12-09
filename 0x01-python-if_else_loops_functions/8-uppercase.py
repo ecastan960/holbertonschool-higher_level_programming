620ae50b6f8b819336d908f23108d822d63531bf
ecastan960 <ecastan1@eafit.edu.co>
2020-12-08 21:47:48 -0500

    09|Fix string manipulation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 def uppercase(str):
     for letter in str:
+        x = ord(letter)
         if letter >= 'a' and letter <= 'z':
-            letter = chr(ord(letter) - 32)
+            letter = chr(x - 32)
         print("{}".format(letter), end='')
     print()
