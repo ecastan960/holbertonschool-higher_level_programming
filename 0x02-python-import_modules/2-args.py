e9907b96980925a077d84f4a42da472ceec40512
ecastan960 <ecastan1@eafit.edu.co>
2020-12-10 12:59:00 -0500

    01|Fix number of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-number = len(sys.argv) - 1
-if number == 0:
-    print("0 arguments.")
-else:
-    if number == 1:
-        print("1 argument:")
-        print("1: {}:".format(sys.argv[number]))
-    if number > 1:
-        print("{} arguments:".format(number))
-        for x in range(number):
-            print("{}: {}".format(x + 1, sys.argv[x + 1]))
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    else:
+        if number == 1:
+            print("1 argument:")
+            print("1: {}:".format(sys.argv[number]))
+        if number > 1:
+            print("{} arguments:".format(number))
+            for x in range(number):
+                print("{}: {}".format(x + 1, sys.argv[x + 1]))
