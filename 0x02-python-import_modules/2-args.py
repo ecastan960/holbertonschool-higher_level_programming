0a4d910a2b177ba7db1691aa19f6cc354cd7d5e2
ecastan960 <ecastan1@eafit.edu.co>
2020-12-10 12:49:28 -0500

    00|First try

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+number = len(sys.argv) - 1
+if number == 0:
+    print("0 arguments.")
+else:
+    if number == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[number]))
+    if number > 1:
+        for x in range(number):
+            print("{}: {}".format(x + 1, sys.argv[x + 1]))
