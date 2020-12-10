fb5863c3cf778f2723c6fd69d5238006d67103d0
ecastan960 <ecastan1@eafit.edu.co>
2020-12-10 13:06:36 -0500

    02|Add line to avoid execution

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-number = len(sys.argv) - 1
-add = 0
-if number == 0:
-    print("0")
-elif number == 1:
-    print(sys.argv[number])
-else:
-    for x in range(number):
-        add = add + int(sys.argv[x + 1])
-    print(add)
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv) - 1
+    add = 0
+    if number == 0:
+        print("0")
+    elif number == 1:
+        print(sys.argv[number])
+    else:
+        for x in range(number):
+            add = add + int(sys.argv[x + 1])
+        print(add)
