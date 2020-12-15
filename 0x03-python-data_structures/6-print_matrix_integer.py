d507a9940318a4727c8a07b4b7bd916c806287de
ecastan960 <ecastan1@eafit.edu.co>
2020-12-15 17:21:03 -0500

    test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print()
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             if y == len(matrix[x]) - 1:
