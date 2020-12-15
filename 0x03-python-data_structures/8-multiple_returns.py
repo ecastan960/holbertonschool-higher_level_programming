5d3c8adac75eea79ae559929efce70309d81f012
ecastan960 <ecastan1@eafit.edu.co>
2020-12-15 18:50:41 -0500

    15|First test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return 0, None
+        return None, None
     else:
         return len(sentence), sentence[0]
