4924067813d08f5d833fee240fa9905a7e6549bc
ecastan960 <ecastan1@eafit.edu.co>
2020-12-15 15:59:34 -0500

    10|Add edge case

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,6 +17,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
             new_tuple1 = (tuple_a[0], tuple_a[1])
             new_tuple2 = (tuple_b[0], tuple_b[1])
             return tuple(p+q for p, q in zip(new_tuple1, new_tuple2))
+        elif len(tuple_a) == 1 and len(tuple_b) == 1:
+            new_tuple1 = (tuple_a[0], 0)
+            new_tuple2 = (tuple_b[0], 0)
+            return tuple(p+q for p, q in zip(new_tuple1, new_tuple2))
         elif len(tuple_a) == 0 and len(tuple_b) == 0:
             new_tuple1 = (0, 0)
             new_tuple2 = (0, 0)
