5f628623c9e45bf5733d62d46f203ee2f14b1c9c
ecastan960 <ecastan1@eafit.edu.co>
2020-12-15 14:19:50 -0500

    03|test edge case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     y = 0
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print()
     for x in range(len(matrix)):
-        for y in range(len(matrix[y])):
-            if y == len(matrix[y]) - 1:
-                print("{}".format(matrix[x][y]), end="")
+        for y in range(len(matrix[x])):
+            if y == len(matrix[x]) - 1:
+                print("{}".format(matrix[x][y]))
             else:
                 print("{} ".format(matrix[x][y]), end="")
-        print()
