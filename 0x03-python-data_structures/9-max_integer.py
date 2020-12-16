3bcedbad7fc70b171ed1dafd4ff48a384d431ffd
ecastan960 <ecastan1@eafit.edu.co>
2020-12-15 19:18:43 -0500

    22|Edge case fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max1 = 0
     if my_list == []:
         return None
+    max1 = my_list[0]
     for x in my_list:
         if x > max1:
             max1 = x
