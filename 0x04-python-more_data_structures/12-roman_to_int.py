49fa542bec928010050e213068e59e5010a8ebde
ecastan960 <ecastan1@eafit.edu.co>
2020-12-17 18:56:55 -0500

    15|Adding dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = 0
+    roman_dictionary = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D': 500, 'M':1000}
     r_string = list(roman_string)
-    for x in r_string
+    for x in r_string:
         if x in r_string:
-            roman = roman + r_string.get(x)
+            roman = roman + roman_dictionary.get(x)
     return roman
