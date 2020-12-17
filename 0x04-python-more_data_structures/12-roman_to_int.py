54813bdc7b347457db19e1a403fd6ad405440f73
ecastan960 <ecastan1@eafit.edu.co>
2020-12-17 18:53:10 -0500

    14|First test

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = 0
+    r_string = list(roman_string)
+    for x in r_string
+        if x in r_string:
+            roman = roman + r_string.get(x)
+    return roman
