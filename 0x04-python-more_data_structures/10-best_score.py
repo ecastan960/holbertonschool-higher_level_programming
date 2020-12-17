6cd6d582b0b44c298b1c9f8e69e03897c5ce7a23
ecastan960 <ecastan1@eafit.edu.co>
2020-12-17 14:33:34 -0500

    11|Fix aux variable

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,16 +2,11 @@
 def best_score(a_dictionary):
     a = 0
     b = ''
-    v = a_dictionary.values()
-    for x in v:
-        if x:
-            c = 1
-        else:
-            c = 0
-    if c == 0:
+    if a_dictionary is None:
         return None
     for y in a_dictionary:
         x = a_dictionary.get(y)
         if x > a:
             b = y
+            a = x
     return b
