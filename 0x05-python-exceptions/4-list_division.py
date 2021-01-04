d524f1e9252d2230c78f2c77159bbd993507f8b3
ecastan960 <ecastan1@eafit.edu.co>
2021-01-04 04:48:39 -0500

    05|First test

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0] * list_length
+    for i in range(list_length):
+        try:
+            new_list[i] = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list[i] = 0
+        except IndexError:
+            print("out of range")
+            new_list[i] = 0
+        except TypeError:
+            print("wrong type")
+        finally:
+            pass
+    return new_list
