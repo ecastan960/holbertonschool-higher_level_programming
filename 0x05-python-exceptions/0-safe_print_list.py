f57d2ae5753fdbdb27c645e0aad13827d6588c36
ecastan960 <ecastan1@eafit.edu.co>
2021-01-04 13:03:28 -0500

    09|second test trying to fix checkers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    c = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end='')
-        print()
-        return i + 1
-    except:
-        print()
-        return i
+            c = c + 1
+        except IndexError:
+            break
+    print()
+    return c
