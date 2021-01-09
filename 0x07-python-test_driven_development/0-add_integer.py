196e6922d8b1222ea84e7b7727be7ce193b2e2a7
ecastan960 <ecastan1@eafit.edu.co>
2021-01-08 23:13:42 -0500

    01|Fixing pep8 issues

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Documentation for a function that add to integers
 """
+
+
 def add_integer(a, b=98):
-    """[summary]
+    """This function adds two integers
 
     Args:
-        a ([type]): [description]
-        b (int, optional): [description]. Defaults to 98.
+        a (int): integer to add
+        b (int, optional): integer to add. Defaults to 98.
 
     Raises:
-        TypeError: [description]
-        TypeError: [description]
+        TypeError: raises when a its not an integer or float
+        TypeError: raises when a its not an integer or float
 
     Returns:
-        [type]: [description]
+        [int]: returns the sum of two integers
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
