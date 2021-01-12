c8899dad467e2ddf33ab1b34800aee79d9c567a1
ecastan960 <ecastan1@eafit.edu.co>
2021-01-11 20:32:26 -0500

    07|fixing pep8 issue

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -141,6 +141,7 @@ class Rectangle:
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """[summary]
 
