6b56bc618a02f1fae83ae7b0a9000cf6dc43c259
ecastan960 <ecastan1@eafit.edu.co>
2021-01-11 20:27:51 -0500

    05|first test

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -141,6 +141,7 @@ class Rectangle:
         Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) != Rectangle:
             raise ("rect_1 must be an instance of Rectangle")
