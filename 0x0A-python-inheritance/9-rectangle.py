ff2aae3789c091fc1cabbd95b918657ce8d2a951
ecastan960 <ecastan1@eafit.edu.co>
2021-01-19 21:56:14 -0500

    09|first test

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Documentation for class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """[summary]
+    """
+    def area(self):
+        """[summary]
+
+        Raises:
+            Exception: [description]
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """[summary]
+
+        Args:
+            name ([type]): [description]
+            value ([type]): [description]
+
+        Raises:
+            TypeError: [description]
+            ValueError: [description]
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """[summary]
+
+    Args:
+        BaseGeometry ([type]): [description]
+    """
+
+    def __init__(self, width, height):
+        """[summary]
+
+        Args:
+            width ([type]): [description]
+            height ([type]): [description]
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """[summary]
+        """
+        return("[Rectangle] {}/{}".format(self.__height, self.__width))
