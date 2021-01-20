0f8cb04996f1185175c7ec0695486913e5f50919
ecastan960 <ecastan1@eafit.edu.co>
2021-01-19 20:38:49 -0500

    03|first try

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Documentation for class MyList
+"""
+
+
+class BaseGeometry:
+    """[summary]
+    """
+    def area(self):
+        """[summary]
+
+        Raises:
+            Exception: [description]
+        """
+        raise Exception("area() is not implemented")
