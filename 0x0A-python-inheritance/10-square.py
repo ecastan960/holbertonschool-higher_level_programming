44d35704e2bf036cf5560356589878224ff151e3
ecastan960 <ecastan1@eafit.edu.co>
2021-01-19 22:20:59 -0500

    12|pep fixes

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -63,12 +63,28 @@ class Rectangle(BaseGeometry):
         """
         return("[Rectangle] {}/{}".format(self.__width, self.__height))
 
+
 class Square(Rectangle):
+    """[summary]
+
+    Args:
+        Rectangle ([type]): [description]
+    """
 
     def __init__(self, size):
+        """[summary]
+
+        Args:
+            size ([type]): [description]
+        """
         self.integer_validator("size", size)
-        super().__init__(size,size)
+        super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
         return self.__size * self.__size
