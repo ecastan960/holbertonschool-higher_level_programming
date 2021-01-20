7c3458e853cc293ed51bcfea8f1b8300ffda6bfe
ecastan960 <ecastan1@eafit.edu.co>
2021-01-19 21:30:26 -0500

    08|changing evaluation of type(value)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,7 +25,7 @@ class BaseGeometry:
             TypeError: [description]
             ValueError: [description]
         """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
