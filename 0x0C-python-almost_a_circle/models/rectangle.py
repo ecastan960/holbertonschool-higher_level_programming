383e3dd82265e368099b3892fd735777e2bdc18a
ecastan960 <ecastan1@eafit.edu.co>
2021-01-27 06:41:06 -0500

    06|fix messages

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,10 +68,10 @@ class Rectangle(Base):
             raise ValueError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         if id is not None:
             self.id = id
         else:
