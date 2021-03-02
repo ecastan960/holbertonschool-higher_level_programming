b22569651d8305eb9a73e3e113e7e3519c2430a0
ecastan960 <ecastan1@eafit.edu.co>
2021-03-02 15:18:14 -0800

    16|fix unique command

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- script that creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id  
 (
-	UNIQUE id INT DEFAULT 1,
-	name VARCHAR(256)
+	id INT DEFAULT 1,
+	name VARCHAR(256),
+    UNIQUE KEY un_id (id) 
 );
