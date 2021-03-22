7535fc7002736c96e68858f0c2eeb8883c65aa14
ecastan960 <ecastan1@eafit.edu.co>
2021-03-22 12:24:51 -0500

    05|Character change

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else if (process.argv[2] > 0) {
   const times = process.argv[2];
-  const text = 'x';
+  const text = 'X';
   for (let i = 0; i < times; i++) {
     console.log(text.repeat(times));
   }
