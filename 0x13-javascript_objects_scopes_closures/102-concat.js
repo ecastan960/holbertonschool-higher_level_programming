aeb5b1cf1388a95fc529068f1c71f02f68995e0a
ecastan960 <ecastan1@eafit.edu.co>
2021-03-23 16:45:16 -0500

    15|removing adding new line in output file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
-
 const data1 = fs.readFileSync(fileA, 'utf8');
 const data2 = fs.readFileSync(fileB, 'utf8');
-fs.writeFileSync(fileC, data 1 + data 2, 'utf8');
+fs.writeFileSync(fileC, data1 + data2, 'utf8');
