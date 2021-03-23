c70c03c6e923f2e06a2953deb0c4771d770c422a
ecastan960 <ecastan1@eafit.edu.co>
2021-03-23 16:46:56 -0500

    16|fix new line issue

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,4 +5,5 @@ const fileB = process.argv[3];
 const fileC = process.argv[4];
 const data1 = fs.readFileSync(fileA, 'utf8');
 const data2 = fs.readFileSync(fileB, 'utf8');
-fs.writeFileSync(fileC, data1 + data2, 'utf8');
+const text = `${data1} ${data2}`;
+fs.writeFileSync(fileC, text);
