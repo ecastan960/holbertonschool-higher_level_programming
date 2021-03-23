9ea1655645ff295e37f0b609257f6382b5801a28
ecastan960 <ecastan1@eafit.edu.co>
2021-03-23 16:40:34 -0500

    12|removing unnecesary lines

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,11 +5,7 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-try {
-  const data1 = fs.readFileSync(fileA, 'utf8');
-  const data2 = fs.readFileSync(fileB, 'utf8');
-  const text = `${data1}\n${data2}\n`;
-  fs.writeFileSync(fileC, text);
-} catch (err) {
-  console.error(err);
-}
+const data1 = fs.readFileSync(fileA, 'utf8');
+const data2 = fs.readFileSync(fileB, 'utf8');
+const text = `${data1}\n${data2}\n`;
+fs.writeFileSync(fileC, text);
