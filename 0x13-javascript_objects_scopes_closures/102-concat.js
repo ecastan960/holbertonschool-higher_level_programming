7b9db87974149d7656485c83f1123c11f0d17f6e
ecastan960 <ecastan1@eafit.edu.co>
2021-03-23 16:38:05 -0500

    12|adding new line at the end of file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,10 +6,10 @@ const fileB = process.argv[3];
 const fileC = process.argv[4];
 
 try {
-  const data1 = fs.readFileSync(fileA, 'utf8')
-  const data2 = fs.readFileSync(fileB, 'utf8')
-  const text = `${data1}\n${data2}`;
-  const data3 = fs.writeFileSync(fileC, text);
+  const data1 = fs.readFileSync(fileA, 'utf8');
+  const data2 = fs.readFileSync(fileB, 'utf8');
+  const text = `${data1}\n${data2}\n`;
+  fs.writeFileSync(fileC, text);
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
