023778a8bcfa2b5d95aefa0d5688737596c1098d
ecastan960 <ecastan1@eafit.edu.co>
2021-03-23 16:19:51 -0500

    09|Firts test

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, (err, data) => {
+  if (err) throw err;
+
+  const file1 = data.toString();
+  fs.appendFile(fileC, file1, (err) => {
+    if (err) throw err;
+  });
+});
+
+fs.readFile(fileB, (err, data) => {
+  if (err) throw err;
+
+  const file2 = data.toString();
+  fs.appendFile(fileC, `\n${file2}`, (err) => {
+    if (err) throw err;
+  });
+});
