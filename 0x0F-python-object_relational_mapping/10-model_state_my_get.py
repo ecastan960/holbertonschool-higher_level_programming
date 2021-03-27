28e8a0adb7b7cbe661b403de8c45fde3e18924a3
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 08:44:23 -0500

    21|change argv position

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,14 +6,13 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
-username = argv[1]
-password = argv[2]
-d_name = argv[3]
-state_name = argv[4]
-
 if __name__ == '__main__':
     """This code its not executed if imported
     """
+    username = argv[1]
+    password = argv[2]
+    d_name = argv[3]
+    state_name = argv[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, d_name),
                            pool_pre_ping=True)
