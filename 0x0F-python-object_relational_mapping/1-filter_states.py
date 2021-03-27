fee3a281117a1435708078a1c4e844fe5d1f7945
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 07:43:09 -0500

    08|change argv position

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,13 @@ N (upper N) from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-d_name = argv[3]
-
 if __name__ == '__main__':
     """This code its not executed if imported
     """
+
+    username = argv[1]
+    password = argv[2]
+    d_name = argv[3]
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password, db=d_name)
     c = conn.cursor()
