8301b1be60860f8b85cdc629c7aa0264a3b4059d
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 07:27:43 -0500

    04|fix comments issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,8 @@ password = argv[2]
 d_name = argv[3]
 
 if __name__ == '__main__':
-    """This code its not executed if imported
+    """
+    This code its not executed if imported
     """
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password, db=d_name,
