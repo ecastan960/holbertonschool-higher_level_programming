7dbd9defb86f0c5ab531174b1a341b0b4ca26067
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 17:38:05 -0500

    25|Adding relationship

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -18,4 +18,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    city_childre = relationship("City")
+    city_children = relationship("City")
