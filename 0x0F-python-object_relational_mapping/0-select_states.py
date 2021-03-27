28f194897904ae0dd792a1e5f16326804c6f3a62
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 07:42:14 -0500

    07|change argv position

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-d_name = argv[3]
-
 if __name__ == '__main__':
+
+    username = argv[1]
+    password = argv[2]
+    d_name = argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password, db=d_name,
