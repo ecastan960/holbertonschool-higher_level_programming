99d8d88d111213ffa4c8155a37fc48647671f78e
ecastan960 <ecastan1@eafit.edu.co>
2021-03-27 17:36:43 -0500

    25|Adding relationship

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@ and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -17,3 +18,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    city_childre = relationship("City")
