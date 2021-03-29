53586a7b89f675525a353478d2b93ce8e1d8e0c4
ecastan960 <ecastan1@eafit.edu.co>
2021-03-28 23:45:17 -0500

    31|Fix cascade issues

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,4 +18,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    city = relationship('City', cascade="all, delete, delete-orphan")
+    city = relationship('City', cascade="delete")
