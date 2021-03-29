a6b626e039309affddbb06b115b96c23117be2cc
ecastan960 <ecastan1@eafit.edu.co>
2021-03-28 23:48:43 -0500

    32|modigy state_id arguments

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -16,5 +16,5 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
     state = relationship('State')
