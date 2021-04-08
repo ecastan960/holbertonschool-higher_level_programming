7e40a6bb9274062d2225895eef2ab34732a49e60
ecastan960 <ecastan1@eafit.edu.co>
2021-04-08 17:09:34 -0500

    12|Fix options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "$1" -X OPTIONS -L
+curl -s -X OPTIONS "$1"
