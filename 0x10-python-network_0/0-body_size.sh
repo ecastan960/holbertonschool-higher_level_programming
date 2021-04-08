dac445bbe9d9e80ed69cba1928d6ae0c9ceb0020
ecastan960 <ecastan1@eafit.edu.co>
2021-04-08 16:23:14 -0500

    01|Fix output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl 127.0.0.1:5000 -sI | grep -i 'content-length' | cut -d ':' -f2
+curl 127.0.0.1:5000 -sI | grep -i 'content-length:' | cut -d ' ' -f2
