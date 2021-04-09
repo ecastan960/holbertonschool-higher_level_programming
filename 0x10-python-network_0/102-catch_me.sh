7c5596f1a7cc29359a98cda9ad6edb1ab56afc03
ecastan960 <ecastan1@eafit.edu.co>
2021-04-08 22:37:17 -0500

    27|First test

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -s -H "Content-Type: application/json" -X POST -d @"$2" "$1"
+curl -sL  "$1" -X PUT -d "user_id=98"
