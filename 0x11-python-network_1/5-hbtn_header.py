7369a293a0acb4432c66a6c636fd9e41081a767a
ecastan960 <ecastan1@eafit.edu.co>
2021-04-09 15:17:42 -0500

    08|add confirmation of existence

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     """Don't execute if imported
     """
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    if 'X-Request-Id' in r.headers:
+        print(r.headers['X-Request-Id'])
