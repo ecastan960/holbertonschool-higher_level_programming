f18fd6daa60e0920d8aa5431dd506be163d1af65
ecastan960 <ecastan1@eafit.edu.co>
2021-04-09 17:08:51 -0500

    12|Modifing conditioning

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     """
     url = sys.argv[1]
     r = requests.get(sys.argv[1])
-    if r.status_code < 400:
+    if r.status_code == 200:
         print(r.text)
     else:
         print("Error code: {} ".format(r.status_code))
