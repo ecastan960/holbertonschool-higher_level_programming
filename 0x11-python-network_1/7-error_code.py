5fbbcabefb6eea69088918a5922af930a8d3ff04
ecastan960 <ecastan1@eafit.edu.co>
2021-04-09 16:56:17 -0500

    10|First test

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    """Don't execute if imported
+    """
+    url = sys.argv[1]
+    r = requests.get(sys.argv[1])
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {} ".format(r.status_code))
