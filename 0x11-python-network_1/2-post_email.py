76219b5720e684e86a69b4851aedfdf266fade8e
ecastan960 <ecastan1@eafit.edu.co>
2021-04-09 14:17:38 -0500

    02|First test

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL,
+sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response.
+"""
+
+import sys
+from urllib import parse, request
+
+if __name__ == "__main__":
+    """Don't execute if imported
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = parse.urlencode(email)
+    data = data.encode('utf-8')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print(html)
