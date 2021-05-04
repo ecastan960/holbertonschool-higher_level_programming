0d6a39014b90960027c4fcc8bd6ac4c4330bec99
ecastan960 <ecastan1@eafit.edu.co>
2021-05-04 13:31:08 -0500

    02|Update file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,8 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response, body) {
-  console.error('error:', error);
+  if (error) {
+    console.error('error:', error);
+  }
   console.log('code:', response && response.statusCode);
 });
