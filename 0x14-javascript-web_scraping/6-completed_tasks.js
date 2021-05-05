1ac82b4e3b8d3679675a7d2d578a9ee205369e03
ecastan960 <ecastan1@eafit.edu.co>
2021-05-04 20:04:11 -0500

    00|First test

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+const output = {};
+let n = 0;
+let count = 0;
+let user = 1;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const object = JSON.parse(body);
+  while (object[n]) {
+    console.log(object[n].userId);
+    console.log(user);
+    console.log(n);
+    console.log('--------------');
+    while (object[n].userId === user) {
+      console.log('--------after');
+      if (object[n].completed === true) {
+        count = count + 1;
+      }
+      n = n + 1;
+      if (n === 200) {
+        break;
+      }
+    }
+    output['' + user] = count;
+    user = user + 1;
+    count = 0;
+  }
+  console.log(output);
+});
